fe2370eac265ed901c42b07a3f598edb2e2652ff
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-16 20:48:51 -0300

    model cliente

--- index.js
+++ index.js
@@ -8,3 +8,18 @@ const sequelize = new Sequelize('db_projeto', 'root', '', {
     dialect: 'mysql'
 })
 
+// Definição de tabelas (modelo) de clientes.
+const Cliente = sequelize.define('Cliente', {
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    }, 
+    telefone: {
+        type: DataTypes.STRING
+    }
+})
